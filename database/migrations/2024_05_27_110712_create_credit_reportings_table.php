60b4ddeac22ee1d7150cf59f279f3d09fc6fcb2f
alimarchal <alirazamarchal@hotmail.com>
2024-10-09 12:22:57 +0500

    added updated by in credit reporting

--- database/migrations/2024_05_27_110712_create_credit_reportings_table.php
+++ database/migrations/2024_05_27_110712_create_credit_reportings_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->foreignId('branch_id')->nullable()->constrained();
+            $table->foreignId('updated_by')->nullable()->constrained('users')->nullOnDelete();
             $table->string('name')->nullable();
             $table->string('national_id_cnic')->nullable();
             $table->string('comments')->nullable();
